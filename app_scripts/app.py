71669a5041a72418e122dafdc692892e2183ba96
Jmoreno1217 <a220200161@gmail.com>
2023-05-09 17:43:41 -0700

    Añadido login.html (unicamente diseño)

--- app_scripts/app.py
+++ app_scripts/app.py
@@ -10,7 +10,8 @@ def handle_context():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    #return render_template("index.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
